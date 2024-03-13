7c9fc32343e6bd0d42249c6d0e776f0b5f70cc33
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-03-13 18:41:58 +0900

    feature: add queue

---
+++ Queue/Queue.ts
@@ -0,0 +1,27 @@
+import LinkedList from "../LinkedList/linkedList";
+
+class Queue<T> {
+	public list: LinkedList<T>;
+	public maxSize: number;
+
+	constructor(match: () => boolean, destroy: () => T | null, maxSize) {
+		this.list = new LinkedList<T>(match, destroy);
+		this.maxSize = maxSize;
+	}
+
+	public isEmpty(): boolean {
+		return this.list.size === 0;
+	}
+	public isFull(): boolean {
+		return this.list.size === this.maxSize;
+	}
+
+	public enqueue(data: T): number {
+		if (this.isFull()) console.log("queue is full")
+		if (this.list.insNext(null, data)) return 0;
+	}
+	public dequeue(): T | null {
+		if (this.isEmpty()) console.log("queue is empty")
+		return this.list.remNext(null);
+	}
+}
